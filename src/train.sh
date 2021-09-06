8f30087ff91ebf93897d13eee4aa9b9d5e81e390
ujiuji1259 <56721415+ujiuji1259@users.noreply.github.com>
2021-09-06 16:50:11 +0900

    Update train.sh

--- src/train.sh
+++ src/train.sh
@@ -2,8 +2,8 @@ python train.py \
     --input_path /path/to/Target_Category \
     --model_path /path/to/model_directory \
     --lr 1e-5 \
-    --bsz 32 \
-    --epoch 50 \
+    --bsz 4 \
+    --epoch 5 \
     --grad_acc 1 \
     --grad_clip 1.0 \
     --seed 0 \
